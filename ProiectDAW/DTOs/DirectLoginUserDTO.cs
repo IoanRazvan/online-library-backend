20b31f9231f35c30c356a6f48cacc92cacd7e3db
Ioan Rusu <ioanrazvanrusu@gmail.com>
2021-12-09 11:37:47 +0200

    Added DataAnnotations to DirectLoginUserDTO class

--- ProiectDAW/DTOs/DirectLoginUserDTO.cs
+++ ProiectDAW/DTOs/DirectLoginUserDTO.cs
@@ -1,18 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace ProiectDAW.DTOs
 {
     public class DirectLoginUserDTO
     {
+        [Required]
         public string FirstName { get; set; }
 
+        [Required]
         public string LastName { get; set; }
 
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
+        [Required]
+        [MinLength(6)]
         public string Password { get; set; }
     }
 }
