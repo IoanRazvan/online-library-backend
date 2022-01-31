d5635056362b2570f9ee9142bb57e9af53f6c377
Ioan Rusu <ioanrazvanrusu@gmail.com>
2022-01-31 21:08:40 +0200

    Updated Review table

---
+++ ProiectDAW/Migrations/20220131190416_AddedDateOfPostingToReviews.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProiectDAW.Migrations
+{
+    public partial class AddedDateOfPostingToReviews : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfPosting",
+                table: "Reviews",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateOfPosting",
+                table: "Reviews");
+        }
+    }
+}
--- ProiectDAW/Models/Review.cs
+++ ProiectDAW/Models/Review.cs
@@ -16,9 +16,10 @@ namespace ProiectDAW.Models
 
         public User Reviewer { get; set; }
 
-        [Range(1.0, 5.0)]
         public float Score { get; set; }
 
         public string Comment { get; set; }
+
+        public DateTime DateOfPosting { get; set; }
     }
 }
