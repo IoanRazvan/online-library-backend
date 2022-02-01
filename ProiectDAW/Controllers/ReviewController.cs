8bf399a3f56f8e1a0c932504bbdb20bcac0835b6
Ioan Rusu <ioanrazvanrusu@gmail.com>
2022-02-01 22:38:21 +0200

    Added Review controller

---
+++ ProiectDAW/Controllers/ReviewController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProiectDAW.DTOs;
+using ProiectDAW.Security.Attributes;
+using ProiectDAW.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace ProiectDAW.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewController : ControllerBase
+    {
+        private readonly IReviewService _service;
+
+        public ReviewController(IReviewService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{bookId}")]
+        [Authorization]
+        public async Task<IActionResult> GetBookReviewsPaged([FromRoute] Guid bookId, [FromQuery] int page, [FromQuery] int pageSize)
+        {
+           return Ok(await _service.FindByBookIdPaged(bookId, page, pageSize));
+        }
+
+        [HttpGet("has-reviewed/{bookId}")]
+        [Authorization]
+        public async Task<IActionResult> HasAlreadyReviewed([FromRoute] Guid bookId)
+        {
+            return Ok(await _service.ExistsByBookIdAndReviewerPrincipal(bookId));
+        }
+
+        [HttpPost("{bookId}")]
+        public async Task<IActionResult> SaveReview([FromRoute] Guid bookId, [FromBody] ReviewDataDTO reviewData)
+        {
+            return Ok(await _service.Save(bookId, reviewData));
+        }
+    }
+}
